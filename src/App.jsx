4a45d552aa5d1bdb76ececc92bc5a0eea3dd5471
Adrian <serbanedward@yahoo.com>
2024-10-30 14:36:32 +0100

    frontend deploy

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Form from "./comp/Form";
+
+function App() {
+  return (
+    <>
+      <h1>User registration</h1>
+      <Form />
+    </>
+  );
+}
+
+export default App;
---
+++ src/comp/Form.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+
+const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [details, setDetails] = useState([]);
+
+  const userData = { name, email, password };
+  //console.log("User data", userData);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "https://backend-2024-pvka.onrender.com/api/submit/",
+        userData
+      );
+      //console.log(response);
+      //console.log(response.data);
+      setDetails(response.data.details);
+      setName("");
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      console.log(error);
+    }
+    //console.log("Clicked");
+  };
+  return (
+    <div>
+      <form>
+        <label>
+          Name: &nbsp;
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Email: &nbsp;
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password: &nbsp;
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <br />
+        <input type="submit" value="submit" onClick={handleSubmit} />
+      </form>
+      {details.map((user) => {
+        return (
+          <div key={user._id}>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Form;
